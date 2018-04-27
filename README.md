1fdbe07a7392962bd0bbdf05e63341720e7a2b7c
JiwGle-Macbook <jiw.surachat@gmail.com>
2018-04-27 10:33:10 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Docker PHP Server Monitor
+
+
+Run docker
+```
+docker-compose up -d
+```
+
+source code download : https://www.phpservermonitor.org/download/
+runing on : localhost
+username : admin
+pasword : 1234
---
+++ www/config.php
@@ -0,0 +1,8 @@
+<?php
+define('PSM_DB_HOST', 'db');
+define('PSM_DB_PORT', '3306');
+define('PSM_DB_NAME', 'monitor');
+define('PSM_DB_USER', 'web');
+define('PSM_DB_PASS', '1234');
+define('PSM_DB_PREFIX', 'psm_');
+define('PSM_BASE_URL', 'http://localhost');
